63e40fb9dfd2e9a9f1af468e0f08ebab6d2b8574
Robson Mesquita Gomes <robson.mesquita56@gmail.com>
2025-10-07 20:31:56 -0300

    ✨ feat(core): Add base files

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Display Oled BitDogLab
+
+O projeto consiste em controlar o display Oled na placa BitDogLab (Pico W) apresentando textos, linha e imagem (logo).
+
+> **Importante**: esse projeto foi criado com a versão 1.5.1 do SDK
+
+## Formato da logo
+
+A logo precisa ser do tamanho 128x64 pixels e precisa ser tratada para ser convertida em um array antes de ser enviada ao display. Nesse projeto foi utilizado o site https://javl.github.io/image2cpp/ para conversão da imagem.
+
+## Demo
+
+![](demo/demo.jpeg)
---
+++ display_oled.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "inc/ssd1306.h"
+#include "hardware/i2c.h"
+#include "assets/logo.h"
+
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
+
+int main()
+{
+    stdio_init_all();
+
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
+
+    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    // zera o display inteiro
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+restart:
+
+    char *text[] = {
+        "  Ola, mundo!   ",
+        "  Robson Gomes   "};
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(3000);
+    
+    // Parte do código para exibir a linha no display (algoritmo de Bresenham)
+    ssd1306_draw_line(ssd, 10, 10, 100, 50, true);
+    render_on_display(ssd, &frame_area);
+
+    sleep_ms(2000);
+
+    ssd1306_t ssd_bm;
+    ssd1306_init_bm(&ssd_bm, 128, 64, false, 0x3C, i2c1);
+    ssd1306_config(&ssd_bm);
+
+    ssd1306_draw_bitmap(&ssd_bm, logo);
+
+    while (true)
+    {
+        sleep_ms(1000);
+    }
+
+    return 0;
+}
